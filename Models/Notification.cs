bdc208c178b101c875c2de971be1b79510cdc624
Joakim N <92833271+Echoinstrum@users.noreply.github.com>
2025-05-30 09:22:40 +0200

    Intitial setup for NotificationService microservice

---
+++ Models/Notification.cs
@@ -0,0 +1,9 @@
+﻿namespace Ventixe.NotificationService.Models;
+
+public class Notification
+{
+    public int Id { get; set; }
+    public int EventId { get; set; }
+    public DateTime SentAt { get; set; } = DateTime.UtcNow;
+    public string Message { get; set; } = string.Empty;
+}
