e00d1d00547eff449ca5050e2dc8c9a8f0579a91
Joakim N <92833271+Echoinstrum@users.noreply.github.com>
2025-05-30 11:29:09 +0200

    controller and service for Notifications are setup

---
+++ Controllers/NotificationsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotificationServiceClass = Ventixe.NotificationService.Services.NotificationService;
+
+namespace Ventixe.NotificationService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NotificationsController : Controller
+{
+    private readonly NotificationServiceClass _notificationService;
+    private readonly ILogger<NotificationsController> _logger;
+
+    public NotificationsController(NotificationServiceClass notificationService, ILogger<NotificationsController> logger)
+    {
+        _notificationService = notificationService;
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public IActionResult Notify([FromBody] int eventId)
+    {
+        _logger.LogInformation("📥 POST /api/notifications reached with eventId: {EventId}", eventId);
+        _notificationService.SendNotification(eventId);
+        return Ok();
+    }
+}
