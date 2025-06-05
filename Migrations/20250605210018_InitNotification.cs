c48854da946defda49d8df725987e89eb5fe22b9
Joakim N <92833271+Echoinstrum@users.noreply.github.com>
2025-06-06 00:23:36 +0200

    added handling of CORS for azure web address

---
+++ Migrations/20250605210018_InitNotification.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ventixe.NotificationService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitNotification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
